6b232fb306a3c6c3bb7be3543fb22d4350c163d2
cristian536 <cdllanosp@libertadores.edu.co>
2020-05-23 01:25:16 -0500

    aprendiendo

---
+++ aprende-hooks/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <header className ="header">
+      <div className= "wrapper">
+        <div  className ="header-grid">
+          </div>
+          <h1>social media desboard</h1>
+          <p className ="header-total">total de followers: 23, 004</p>
+          </div>
+          <div className="dark-mode">
+          <p className ="dark-mode">dark Mode</p>
+          <input type="checkbox" className="checkbox" id="checkbox"/>
+          <label className="switch" htmlFor ="checkbox">
+
+          </label>
+      
+          </div>
+
+        </header>  
+        
+      
+  );
+}
+
+export default App;
